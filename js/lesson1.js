4d1f83e70c5cb526cf580b5dfffdcee8a65ca8f6
Qais Vadia <qais@Qaiss-MBP.broadband>
2023-07-18 00:24:25 +0100

    First JS homework

---
+++ js/lesson1.js
@@ -0,0 +1,38 @@
+/* Question 1 */
+
+let text1 = "hi"
+let text2 = " world"
+let result = text1.concat(text2)
+
+console.log(result);
+
+/* Question 2 */
+
+var string = "hello"
+var number = " 2503"
+
+console.log(string);
+console.log(number);
+
+/* Quesion 3 */
+
+var text01 = "This is "
+var text02 = "a string concatenation"
+var text03 = " of 3 variables"
+
+console.log(text01+text02+text03);
+
+/* Question 4 */
+
+var line1 = "This is the answer to question "
+var num1 = "4"
+
+console.log(line1+num1);
+
+/* Question 3 (revised) */
+
+var jobtitle = "radiographer"
+var YOE = "2"
+var company = "NHS"
+
+console.log("i am a",jobtitle, "with",YOE,"years of experience within the",company);
