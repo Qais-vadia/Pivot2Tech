34cf53250067613fbcd15dcb35d0b0a483aaf8a0
Qais Vadia <qais@Qaiss-MBP.broadband>
2023-08-12 15:29:08 +0100

    Functions with JS

---
+++ js/lesson6.js
@@ -0,0 +1,39 @@
+/* functions
+
+function functionName (){
+
+}
+
+*/
+
+function division(num1,num2){
+    console.log(num1/num2)
+}
+
+division(49,7)
+
+
+function multiply(num1,num2){
+    console.log(num1*num2)
+}
+
+multiply(7,7)
+
+
+function addition(num1,num2){
+    console.log(num1+num2)
+}
+
+addition(5,5)
+
+function subtraction(num1, num2){
+    console.log(num1-num2)
+}
+
+subtraction(15,7)
+
+
+function welcomeMessage(){
+    console.log("Welcome")
+}
+welcomeMessage()
