e0e2d6d11ec1e2429a476dccadc772aabd54b868
Qais <qais.v1@gmail.com>
2024-04-24 18:16:58 +0100

    practicing JS

---
+++ js/practice.js
@@ -0,0 +1,12 @@
+const country = 'Germany';
+const continent = 'Europe';
+let population = 100000000;
+const isIsland = false;
+let language = "german";
+
+
+console.log(typeof isIsland)
+console.log(typeof country);
+console.log(typeof continent);
+console.log(typeof population);
+console.log(typeof language);
